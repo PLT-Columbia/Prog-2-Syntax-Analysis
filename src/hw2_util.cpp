a6cc4853838ca6573d4cb7e5f0145c011183de25
Saikat Chakraborty <saikatc@cs.columbia.edu>
2020-09-30 22:34:56 -0400

    Modify the print utility.

--- src/hw2_util.cpp
+++ src/hw2_util.cpp
@@ -10,8 +10,13 @@ void printCallExprReformatOutput(int lineNo, const std::string& originalSource,
                                  const std::string& formattedSource){
   std::cout << "==================================================\n";
   std::cout << "Function call found at line " << lineNo << "\n";
-  std::cout << "Original Source\n" << originalSource << "\n";
-  std::cout << "Formatted Source\n" << formattedSource << "\n";
+  std::cout << "Original Source\n";
+  std::cout << "---------------------------------------------------\n";
+  std::cout << originalSource << "\n";
+  std::cout << "---------------------------------------------------\n";
+  std::cout << "Formatted Source\n";
+  std::cout << "---------------------------------------------------\n";
+  std::cout << formattedSource << "\n";
   std::cout << "==================================================\n";
 }
 
